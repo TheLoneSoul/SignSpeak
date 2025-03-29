57b111faec7af94f15c49ff2fccb765346e20b27
TheLoneSoul <aabhasb25@gmail.com>
2025-03-29 14:16:28 +0545

    Initialize webcam capture for dataset creation

--- collection_img.py
+++ collection_img.py
@@ -7,3 +7,9 @@ data_path = './data'
 #checking & creating the data directory if it's not existed
 if not os.path.exists(data_path):
     os.makedirs(data_path)
+
+total_class = 26
+data_size = 1000
+
+#open default webcam to capture frames
+cap = cv2.VideoCapture(0)
