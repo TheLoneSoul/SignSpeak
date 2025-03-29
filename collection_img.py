71a3886a2731680ffe78a5a6d93d0ede93301bee
TheLoneSoul <aabhasb25@gmail.com>
2025-03-29 13:50:56 +0545

    added path for data creation

--- collection_img.py
+++ collection_img.py
@@ -1,2 +1,9 @@
 import cv2
 import os
+
+#dataset path
+data_path = './data'
+
+#checking & creating the data directory if it's not existed
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
