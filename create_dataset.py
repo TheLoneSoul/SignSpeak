84464cc126b0e7c5534091e649a01818511bcac7
TheLoneSoul <aabhasb25@gmail.com>
2025-03-30 21:06:38 +0545

    initializing mediapipe hand module with static image processing

--- create_dataset.py
+++ create_dataset.py
@@ -3,3 +3,11 @@ import pickle
 import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
+
+#initialize mediapipe hand module
+mediapipe_hands = mp.solutions.hands
+mediapipe_drawing = mp.solutions.drawing_utils
+mediapipe_drawing_styles = mp.solutions.drawing_styles
+
+#hand object for static image processing with minimum confidence
+hands = mediapipe_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
