e5c0eefdc29a266460b7c8050869901fc3cc73e0
TheLoneSoul <aabhasb25@gmail.com>
2025-04-03 22:22:15 +0545

    Loaded dataset and split into train and test

--- model_train.py
+++ model_train.py
@@ -4,3 +4,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
+
+#Load dataset
+data_dict = pickle.load(open('./dataset.pickle', 'rb'))
+data = np.asarray(data_dict['data'])
+labels = np.asarray(data_dict['labels'])
+
+#Splits into training and testing
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
