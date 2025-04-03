e06af0f7fade71ee937948b0524c165f7bbd9eec
TheLoneSoul <aabhasb25@gmail.com>
2025-04-03 23:23:41 +0545

    Train model and evaluate accuracy

--- model_train.py
+++ model_train.py
@@ -12,3 +12,13 @@ labels = np.asarray(data_dict['labels'])
 
 #Splits into training and testing
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+#Train model using RandomForest
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+
+y_prediction = model.predict(x_test)
+
+#Evaluate accuracy
+score = accuracy_score(y_prediction, y_test)
+print('{}% of image samples were classified Successfully !'.format(score * 100))
